45478beb59f98f576db2e26c55b36d6e5891d51e
Gemechis Elias <44440394+gemechis-elias@users.noreply.github.com>
2023-07-20 15:14:01 +0300

    Update new bot.php

--- bot.php
+++ bot.php
@@ -2,6 +2,7 @@
 $path = "https://api.telegram.org/bot<Your Bot Token>/";
 $update = json_decode(file_get_contents("php://input"), TRUE); 
 
+
 $chatId = $update["message"]["chat"]["id"];
 $message =  $update["message"]["text"];
 $first = $update["message"]["from"]["first_name"];
